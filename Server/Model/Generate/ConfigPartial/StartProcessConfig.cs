5ab232d7f7977e4ccd7d778802616bb50631f5f9
DerekLiu <lc10278@163.com>
2022-04-08 11:47:44 +0800

    迁移多于配置

--- Server/Model/Generate/ConfigPartial/StartProcessConfig.cs
+++ Server/Model/Generate/ConfigPartial/StartProcessConfig.cs
@@ -1,9 +1,76 @@
 using System.Net;
+using System;
+using System.Collections.Generic;
+using MongoDB.Bson.Serialization.Attributes;
+using ProtoBuf;
 
 namespace ET
 {
-    public partial class StartProcessConfig
+
+    [ProtoContract]
+    [Config]
+    public partial class StartProcessConfigCategory : ProtoObject
     {
+        public static StartProcessConfigCategory Instance;
+
+        [ProtoIgnore]
+        [BsonIgnore]
+        private Dictionary<int, StartProcessConfig> dict = new Dictionary<int, StartProcessConfig>();
+
+        [BsonElement]
+        [ProtoMember(1)]
+        private List<StartProcessConfig> list = new List<StartProcessConfig>();
+
+        public StartProcessConfigCategory()
+        {
+            Instance = this;
+        }
+
+        public override void EndInit()
+        {
+            foreach (StartProcessConfig config in list)
+            {
+                config.EndInit();
+                this.dict.Add(config.Id, config);
+            }
+            this.AfterEndInit();
+        }
+
+        public StartProcessConfig Get(int id)
+        {
+            this.dict.TryGetValue(id, out StartProcessConfig item);
+
+            if (item == null)
+            {
+                throw new Exception($"≈‰÷√’“≤ªµΩ£¨≈‰÷√±Ì√˚: {nameof(StartProcessConfig)}£¨≈‰÷√id: {id}");
+            }
+
+            return item;
+        }
+
+        public bool Contain(int id)
+        {
+            return this.dict.ContainsKey(id);
+        }
+
+        public Dictionary<int, StartProcessConfig> GetAll()
+        {
+            return this.dict;
+        }
+
+        public StartProcessConfig GetOne()
+        {
+            if (this.dict == null || this.dict.Count <= 0)
+            {
+                return null;
+            }
+            return this.dict.Values.GetEnumerator().Current;
+        }
+    }
+
+    public partial class StartProcessConfig : ProtoObject
+    {
+        
         private IPEndPoint innerIPPort;
 
         public long SceneId;
--- Server/Model/Generate/ConfigPartial/StartSceneConfig.cs
+++ Server/Model/Generate/ConfigPartial/StartSceneConfig.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
 using System.Net;
+using System;
+using MongoDB.Bson.Serialization.Attributes;
+using ProtoBuf;
 
 namespace ET
 {
@@ -53,7 +56,68 @@ namespace ET
             }
         }
     }
-    
+
+    [ProtoContract]
+    [Config]
+    public partial class StartSceneConfigCategory : ProtoObject
+    {
+        public static StartSceneConfigCategory Instance;
+
+        [ProtoIgnore]
+        [BsonIgnore]
+        private Dictionary<int, StartSceneConfig> dict = new Dictionary<int, StartSceneConfig>();
+
+        [BsonElement]
+        [ProtoMember(1)]
+        private List<StartSceneConfig> list = new List<StartSceneConfig>();
+
+        public StartSceneConfigCategory()
+        {
+            Instance = this;
+        }
+
+        public override void EndInit()
+        {
+            foreach (StartSceneConfig config in list)
+            {
+                config.EndInit();
+                this.dict.Add(config.Id, config);
+            }
+            this.AfterEndInit();
+        }
+
+        public StartSceneConfig Get(int id)
+        {
+            this.dict.TryGetValue(id, out StartSceneConfig item);
+
+            if (item == null)
+            {
+                throw new Exception($"配置找不到，配置表名: {nameof(StartSceneConfig)}，配置id: {id}");
+            }
+
+            return item;
+        }
+
+        public bool Contain(int id)
+        {
+            return this.dict.ContainsKey(id);
+        }
+
+        public Dictionary<int, StartSceneConfig> GetAll()
+        {
+            return this.dict;
+        }
+
+        public StartSceneConfig GetOne()
+        {
+            if (this.dict == null || this.dict.Count <= 0)
+            {
+                return null;
+            }
+            return this.dict.Values.GetEnumerator().Current;
+        }
+    }
+
     public partial class StartSceneConfig: ISupportInitialize
     {
         public long InstanceId;
